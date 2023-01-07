0d5a9545cc867c1631cd1c9975374ec6003b3742
Terry Pearson <4346459+tlpearson@users.noreply.github.com>
2023-01-07 10:27:53 -0500

    Initial upgrade to .NET 7

--- Apps/PcmHammer/Program.cs
+++ Apps/PcmHammer/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,6 +15,7 @@ namespace PcmHacking
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
--- Apps/VpwExplorer/Program.cs
+++ Apps/VpwExplorer/Program.cs
@@ -15,6 +15,7 @@ namespace PcmHacking
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PcmExplorerMainForm());
         }
